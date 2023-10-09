1cf36ba185ca01297a238f4bdbd26c760ec406f4
ky0422 <ky0422a@gmail.com>
2023-10-09 20:47:33 +0900

    `update`: updated lib

--- swua.rs
+++ swua.rs
@@ -1,5 +1,17 @@
+use std::ffi::{CStr, CString};
+
 #[no_mangle]
 pub extern "C" fn print(x: i64) -> i64 {
     println!("Print in Rust: {}", x);
     0
+}
+
+#[no_mangle]
+pub extern "C" fn print_str(x: *const i8) -> *const i8 {
+    let c_str = unsafe { CStr::from_ptr(x) };
+    let str_slice: &str = c_str.to_str().unwrap();
+    println!("out> {}", str_slice);
+
+    let out_str = CString::new(format!("Hello {} from Rust!", str_slice)).unwrap();
+    out_str.into_raw()
 }
