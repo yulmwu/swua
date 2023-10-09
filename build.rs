ac5ce5a2598e0c84ae4b1a60ee91025a37d362ea
ky0422 <ky0422a@gmail.com>
2023-10-09 20:47:28 +0900

    `update`: updated build script

--- build.rs
+++ build.rs
@@ -1,11 +1,17 @@
+mod swua;
+
 use std::process::Command;
 
 fn main() {
-    Command::new("rustc")
+    println!("cargo:rerun-if-changed=swua.rs");
+
+    let s = Command::new("rustc")
         .args(["swua.rs", "--crate-type=cdylib", "-o"])
         .arg("build/libswua.so")
         .status()
         .unwrap();
+
+    assert!(s.success());
 
     println!("cargo:rustc-link-search=native=build");
     println!("cargo:rustc-link-lib=swua");
