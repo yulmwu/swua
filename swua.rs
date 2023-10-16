6eadc0f0527b166462bc88658badd796cf1558c8
ky0422 <ky0422a@gmail.com>
2023-10-16 17:54:11 +0900

    `update`: updated Swua

--- swua.rs
+++ swua.rs
@@ -23,3 +23,11 @@ pub extern "C" fn print_array(x: *const i64, len: i64) -> i64 {
 
     0
 }
+
+#[no_mangle]
+pub extern "C" fn print_struct(x: *const i64) -> i64 {
+    let slice = unsafe { std::slice::from_raw_parts(x, 2) };
+    println!("out> {:?}", slice);
+
+    0
+}
