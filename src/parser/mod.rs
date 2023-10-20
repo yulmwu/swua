4624d53524312e3aff7c2f5e4bf21d91f21c43ad
ky0422 <ky0422a@gmail.com>
2023-10-21 00:11:42 +0900

    `update`: updated Parser

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,70 +1,12 @@
+pub mod error;
 pub mod helpers;
 
+use self::error::{ParseResult, ParsingError};
 use crate::{
     ast::*,
     ident_token_to_string,
     tokenizer::{Lexer, Token, TokenKind},
 };
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct ParsingError {
-    pub kind: ParsingErrorKind,
-    pub position: Position,
-}
-
-impl ParsingError {
-    pub fn new(kind: ParsingErrorKind, position: Position) -> Self {
-        Self { kind, position }
-    }
-
-    pub fn expected_next_token<T>(expected: T, got: T, position: Position) -> Self
-    where
-        T: ToString,
-    {
-        Self::new(
-            ParsingErrorKind::ExpectedNextToken(expected.to_string(), got.to_string()),
-            position,
-        )
-    }
-
-    pub fn expected_ty<T>(expected: T, position: Position) -> Self
-    where
-        T: ToString,
-    {
-        Self::new(ParsingErrorKind::ExpectedTy(expected.to_string()), position)
-    }
-
-    pub fn expected_expression<T>(expected: T, position: Position) -> Self
-    where
-        T: ToString,
-    {
-        Self::new(
-            ParsingErrorKind::ExpectedExpression(expected.to_string()),
-            position,
-        )
-    }
-
-    pub fn unexpected_token<T>(token: T, position: Position) -> Self
-    where
-        T: ToString,
-    {
-        Self::new(
-            ParsingErrorKind::UnexpectedToken(token.to_string()),
-            position,
-        )
-    }
-}
-
-#[derive(Debug, Clone, PartialEq)]
-#[rustfmt::skip]
-pub enum ParsingErrorKind {
-    ExpectedNextToken(String, String),
-    ExpectedTy(String),
-    ExpectedExpression(String),
-    UnexpectedToken(String),
-}
-
-pub type ParseResult<T> = Result<T, ParsingError>;
 
 #[derive(Debug, Default)]
 pub struct Parser<'a> {
