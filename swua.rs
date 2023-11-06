d4366cf5b16cd36501d83f10c95f38aa1eccdd12
ky0422 <ky0422a@gmail.com>
2023-11-06 23:00:18 +0900

    `update`: updated Standard Library

--- swua.rs
+++ swua.rs
@@ -31,3 +31,21 @@ pub extern "C" fn print_struct(x: *const i64, len: i64) -> i64 {
 
     0
 }
+
+#[no_mangle]
+pub extern "C" fn to_string(x: i64) -> *const i8 {
+    let out_str = CString::new(format!("{x}")).unwrap();
+    out_str.into_raw()
+}
+
+#[no_mangle]
+pub extern "C" fn concat_str(x: *const i8, y: *const i8) -> *const i8 {
+    let x_str = unsafe { CStr::from_ptr(x) };
+    let x_slice: &str = x_str.to_str().unwrap();
+
+    let y_str = unsafe { CStr::from_ptr(y) };
+    let y_slice: &str = y_str.to_str().unwrap();
+
+    let out_str = CString::new(format!("{x_slice}{y_slice}")).unwrap();
+    out_str.into_raw()
+}
