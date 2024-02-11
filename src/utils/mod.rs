e8c302117ab28419bdd56bac383aa1394951eea3
ky0422 <ky0422a@gmail.com>
2024-02-12 00:30:26 +0900

    `update`: updated Utils

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,1 +1,9 @@
+use std::hash::{DefaultHasher, Hash, Hasher};
+
 pub mod btreemap2;
+
+pub fn hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+}
